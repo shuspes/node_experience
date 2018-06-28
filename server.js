8ea8b0b44edd7ce226202bce8ab2ae9fe3d5d610
Alexandr Spesivtsev <alexandr_spesivtsev@epbyminw1959.minsk.epam.com>
2018-06-28 14:37:29 +0300

    create http server.

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const port = 3000;
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }).listen(port);
