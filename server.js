1f29d821297a4cbb92fd6d72c7e6bcb38c882d1c
Alexandr Spesivtsev <alexandr_spesivtsev@epbyminw1959.minsk.epam.com>
2018-06-29 12:47:38 +0300

    create entrance app file. beautify http server module.

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const http = require("http");
 
 const port = 3000;
 
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }).listen(port);
+const serverHandler = (request, response) => {
+  console.log("server handled");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+};
+
+const startHTTPServer = () => {
+  const server = http.createServer(serverHandler);
+  server.listen(port);
+};
+
+exports.start = startHTTPServer;
+
