7da74fe3b994d666e035175dc076fc7dc72848bc
Alexandr Spesivtsev <Alexandr_Spesivtsev@epam.com>
2018-06-29 13:57:53 +0300

    add url module to server.

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const http = require("http");
+const url = require("url");
 
 const port = 3000;
 
-const serverHandler = (request, response) => {
+const onRequest = (request, response) => {
   console.log("server handled");
+  const requestedURL = url.parse(request.url);
+  console.log("requestedURL", requestedURL);
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
 };
 
 const startHTTPServer = () => {
-  const server = http.createServer(serverHandler);
+  const server = http.createServer(onRequest);
   server.listen(port);
 };
 
